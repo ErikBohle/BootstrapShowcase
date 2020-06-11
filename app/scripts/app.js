44c6e7ad9deea66f2e061de912e546560a710c0f
erikbohle <55728992+ErikBohle@users.noreply.github.com>
2020-06-11 02:35:44 +0200

    Added filter functionality

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,21 +1,67 @@
-$('#exampleModal').on('show.bs.modal', event => {
+
+//modals
+$("#exampleModal").on("show.bs.modal", (event) => {
   var button = $(event.relatedTarget);
   var modal = $(this);
-  // Use above variables to manipulate the DOM
-  
 });
 
-var $mediaElements = $('.media');
+//filter products
+var $mediaElements = $(".product");
+var min = "";
+var max = "";
+var filterVal = "";
 
-$('.filter_link').click(function(e){
-    e.preventDefault();
-    // get the category from the attribute
-    var filterVal = $(this).data('filter');
+$.fn.test = function(){
+  $(".product")
+  .filter(function () {
+    $mediaElements.show();
+    return $(this).data("price") < min || $(this).data("price") > max;
+  })
+  .hide();};
 
-    if(filterVal === 'all'){
-      $mediaElements.show();
-    }else{
-       // hide all then filter the ones to show
-       $mediaElements.hide().filter('.' + filterVal).show();
-    }
+
+$(".filter_link").click(function (e) {
+  e.preventDefault();
+  // get the category from the attribute
+  // var filterVal = $(".product[data-price='15']")
+  var filterVal = $(this).val();
+  console.log(filterVal, "filtervalue");
+  console.log($mediaElements, "Meelem");
+
+  if (filterVal == "all") {
+    console.log("hi");
+    
+    $mediaElements.show();
+  } else if (filterVal === "0-20") {
+    // $($mediaElements[i]).data("price") < 20 || $($mediaElements[i]).data("price") > 0
+    console.log($($mediaElements).data("price"));
+    min = 0;
+    max = 20;
+    $(".product").test();
+  } else if (filterVal === "20-40") {
+    // $($mediaElements[i]).data("price") < 20 || $($mediaElements[i]).data("price") > 0
+    console.log($($mediaElements).data("price"));
+    min = 20;
+    max = 40;
+    $(".product").test();
+  } else if (filterVal === "40-60") {
+    // $($mediaElements[i]).data("price") < 20 || $($mediaElements[i]).data("price") > 0
+    console.log($($mediaElements).data("price"));
+    min = 40;
+    max = 60;
+    $(".product").test();
+  } else if (filterVal === "60-100") {
+    // $($mediaElements[i]).data("price") < 20 || $($mediaElements[i]).data("price") > 0
+    console.log($($mediaElements).data("price"));
+    min = 60;
+    max = 100;
+    $(".product").test();
+  }
 });
+//  for (i = 0; i < $mediaElements; i++) {
+
+// if ([5,6,7,8,9].indexOf(this.currSlideId) > -1) {
+//   $('.accolades').addClass('current');
+// } else {
+//   $('.accolades').removeClass('current');
+// }
